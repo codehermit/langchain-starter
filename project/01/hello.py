bed5736b9c23590405c3d47697055328a9b4ee50
codehermit <lixingying@gmail.com>
2025-12-11 23:40:31 +0800

    first commit

---
+++ project/01/hello.py
@@ -0,0 +1,17 @@
+import os
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv("API_KEY"))
+print(os.getenv("BASE_URL"))
+
+model = init_chat_model(
+    "qwen-turbo",
+    model_provider="openai",
+    api_key=os.getenv("API_KEY"),
+    base_url=os.getenv("BASE_URL"),
+)
+
+response = model.invoke("用三句话介绍大模型?")
+print(response)
